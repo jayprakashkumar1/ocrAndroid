2be917ca7e4602aa0881978fab6fec5957574089
jayprakashkumar1 <jptu143@gmail.com>
2019-08-14 16:16:59 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,34 @@
+
+import React, {Fragment} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  Colors,
+
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return (
+    <View> 
+      <Text>Hello REACT NATIVE </Text>
+
+      </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+ 
+});
+
+export default App;
